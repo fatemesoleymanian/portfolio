c5754838b9d82d84b3242a4c8fe3b848e524ca3b
fatemesoleymanian <soleymanian.usc@gmail.com>
2025-08-10 14:06:46 +0330

    updated

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="https://drive.google.com/file/d/1TNU08a-X-h1K7dhP-HJN2LMnMi3iebqc/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1LZYOoKPrgMteZbzrpBA8lzc8arqr_1jC/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
